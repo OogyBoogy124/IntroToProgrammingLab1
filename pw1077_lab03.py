0e7a61e692d64f6959dd15c8cb99dbd5b5bd9c35
OogyBoogy124 <pw1077@usnh.edu>
2026-02-13 15:21:58 -0500

    Add files via upload

--- pw1077_lab03.py
+++ pw1077_lab03.py
@@ -57,15 +57,85 @@ def house(t):
     t.left(90)
     t.forward(100)
     t.left(60)
-    t.forward(115)#Making the roof
+    t.forward(116)#Making the roof
     t.left(60)
-    t.forward(115)
+    t.forward(116)
+
+    t.left(90)
+    t.pu()
+    t.forward(50)
+    t.pd()
+    circle(bob,20)#Making Window 1
+    
+    t.left(60)
+    t.pu()
+    t.forward(120)
+    t.right(90)
+    t.forward(12)
+    t.left(90)
+    t.pd()
+    circle(bob, 20)#Making Window 2
+
+def olympic_rings(t):
+    t.color('blue')#Top ring 1
+    circle(t, 40)
+    t.pu()
+    t.forward(60)
+    t.pd()
+    t.color('green') #Top ring 2
+    circle(t, 40)
+    t.pu()
+    t.forward(60)
+    t.pd()
+    t.color('red') #Top ring 3
+    circle(t, 40)
+    t.pu()
+    t.goto(0, 0)#Reseting position for bottom rings
+    t.right(90)
+    t.forward(50)#Moving down
+    t.left(90)
+    t.forward(30)
+    t.pd()
+    t.color('brown') #Bottom ring 1
+    circle(t, 40)
+    t.pu()
+    t.forward(60)
+    t.pd()
+    t.color('yellow') #Bottom ring 2
+    circle(t, 40)
+
+def cube(t):
+    for i in range(2): #Making first rectangle
+        t.forward(100)
+        t.left(90)
+        t.forward(50)
+        t.left(90)
+    t.left(90)
+    t.forward(50)
+    t.left(135)
+    t.forward(50)#Making the first 3D line
+    t.left(45)
+    t.forward(50)
+    t.left(90)
+    for i in range(2): #Making second rectangle
+        t.forward(100)
+        t.left(90)
+        t.forward(50)
+        t.left(90)
+    t.goto(0, 0)
+    t.goto(100,0)#Attaching the rest of the 3D lines
+    t.right(135)
+    t.forward(50)
+    t.right(135)
+    t.forward(50)
+    t.goto(100,50)
     
 #square(bob, 250)
 #polygon(bob, 30, 8)
 #circle(bob, 10)
 #arc(bob, 40, 270)
-
-house(bob)
+#house(bob)
+#olympic_rings(bob)
+cube(bob)
 
 turtle.mainloop()

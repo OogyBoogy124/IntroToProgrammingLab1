b512d27d7818326463ead68e1ef623cdc0049af1
OogyBoogy124 <pw1077@usnh.edu>
2026-03-13 15:16:00 -0400

    Add files via upload

--- pw1077_lab07.py
+++ pw1077_lab07.py
@@ -24,7 +24,7 @@ def reverse_triangle(n): #n = num of rows
             print(chr(64+p), end="")
         print()
         
-def side_ways_arrow(n):
+def side_ways_arrow(n): #n = the starting number
     #Part 4
     #Loop for counting up
     for i in range(n+1): #Loop to repeat the inner loop properly
@@ -46,8 +46,39 @@ def Triangle(row):
     for i in range(row+1):
         print(" " * i, end='') 
         for p in range(0, x-i): 
-            print((row-1)-p, " ", end="")
+            print((row-i)-p, "", end="")
         print()
+
+def mirror_num_diamond(n): #n = what number we're counting up to
+    #Part 6
+    for i in range(n+1):
+        print(" " * (n-i), end='')
+        for p in range(1, i+1):
+            print(p, "", end="")
+        print()
+    for i in range(n+1):
+        print(" " * (i+1), end='')
+        for p in range(1, n-i):
+            print(p, "", end="")
+        print()
+
+def diamond(n):
+    #Part 7
+    for i in range(n+1):
+        print(" " * (n-i), end='')
+        for p in range(0, n-1):
+            if p == 0:
+                print("*")
+            else:
+                print("*", " " * (p+1), "*")
+    
+def star_arrow(n):
+    #Part 8
+    x = n/2
+    y = x/2
+    for i in range(1, int(x+1)):
+        print(" "*(int(x-i)), "*"*(i-1))
+    
 
 def main():
     #Part 1
@@ -61,10 +92,19 @@ def main():
     reverse_triangle(c)
     #Part 4
     d = int(input("Enter number of rows: "))
-    side_ways_arrow(d)"""
+    side_ways_arrow(d)
     #Part 5
     e = int(input("Enter number of rows: "))
     Triangle(e)
+    #Part 6
+    f = int(input("Enter number of rows: "))
+    mirror_num_diamond(f)"""
+    #Part 7
+    #g = int(input("Enter number of rows: "))
+    #diamond(g)
+    #Part 8
+    h = int(input("Enter number of rows: "))
+    star_arrow(h)
 
 if __name__ == "__main__":
     main()

e20b5763e27181b695500dc8a8230e6296eb42d5
OogyBoogy124 <pw1077@usnh.edu>
2026-04-17 15:18:32 -0400

    Add files via upload

--- pw1077_hw2.py
+++ pw1077_hw2.py
@@ -15,15 +15,55 @@ def get_sums(list_int):
 
 def index_of_max_in_range(numbers, low_index, high_index):
     #Part 2
-    a = numbers[low_index,high_index]
-    for i in range(len(a)):
-        if a[i] > a[(i+1)]
-        
+    x = 0
+    y = 0
+    for i in range(low_index, high_index):
+        if numbers[i] > x:
+            x = numbers[i]
+            y = i
+        else:
+            continue
+    print(y)
+
+
+def swap_elements(array, index_a, index_b):
+    #Part 3
+    b = array[index_a]
+    c = array[index_b]
+    array[index_a] = c
+    array[index_b] = b
+    print(array)
+
+def selection_sort(numbers):
+    #Part 4
+    x = 0
+    numbers.append(0)
+    print(numbers)
+    for i in range(len(numbers)):
+        if i + 1 > len(numbers):
+            return
+        else:
+            if numbers[i] > numbers[i+1]:
+                print(i)
+                a = numbers[i]
+                b = numbers[i+1]
+                numbers[i] = b
+                numbers[i+1] = a
+    print(numbers)
 
 def main():
-    part_1_list = [[1,2,4], [1,2,3,6], [1,2,3,4,6,12]]
-    get_sums(part_1_list)
-
+    #Part 1
+    #part_1_list = [[1,2,4], [1,2,3,6], [1,2,3,4,6,12]]
+    #get_sums(part_1_list)
+    #Part 2
+    #part_2_list = [4,7,9,7]
+    #index_of_max_in_range(part_2_list, 0, 4)
+    #part 3
+    #part_3_list = [0,1,2,'a','b',5]
+    #swap_elements(part_3_list,0,4)
+    #Part 4
+    part_4_list = [2,3,1,4]
+    selection_sort(part_4_list)
 
 if __name__ == "__main__":
     main()
